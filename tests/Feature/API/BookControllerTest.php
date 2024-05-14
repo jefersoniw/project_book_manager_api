d6c3db97241029f75bb65da0fe5248a32cca6d7c
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2024-05-14 11:57:05 -0300

    feat: implementando mais testes, para comparação de valores.

--- tests/Feature/API/BookControllerTest.php
+++ tests/Feature/API/BookControllerTest.php
@@ -23,11 +23,24 @@ class BookControllerTest extends TestCase
         $response->assertStatus(200);
         $response->assertJsonCount(4);
 
-        $response->assertJson(function (AssertableJson $json) {
+        $response->assertJson(function (AssertableJson $json) use ($books) {
             $json->whereAllType([
                 '0.id' => 'integer',
                 '0.title' => 'string',
                 '0.isbn' => 'string',
+            ]);
+
+            $json->hasAll([
+                '0.id',
+                '0.title',
+                '0.isbn'
+            ]);
+
+            $book = $books->first();
+            $json->whereAll([
+                '0.id' => $book->id,
+                '0.title' => $book->title,
+                '0.isbn' => $book->isbn
             ]);
         });
     }
